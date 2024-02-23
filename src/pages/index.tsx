8c35c803c88df8321c90f831df93b4f768f6ee53
lamh85 <lamh85@gmail.com>
2024-02-22 23:14:22 -0800

    slider for editing border radius.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+
 export default function Home() {
+  const [borderRadius, setBorderRadius] = useState('0')
+
   return (
     <>
-      <main>hello world</main>
+      <div
+        className="canvas"
+        style={{ height: 500, width: 1000, background: 'grey' }}
+      >
+        <div
+          className="rectangle"
+          style={{
+            height: 100,
+            width: 100,
+            background: 'white',
+            borderRadius: `${borderRadius}%`,
+          }}
+        ></div>
+        <input
+          type="range"
+          value={borderRadius}
+          onChange={(event) => {
+            setBorderRadius(event.target.value)
+          }}
+          min="0"
+          max="50"
+        />
+        <div>{borderRadius}</div>
+      </div>
     </>
   )
 }
